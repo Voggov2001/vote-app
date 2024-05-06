b5a3fbf98f591b9f0948667b0f6c5ad2f868df3b
Ilya Voggov <voggov1@mail.ru>
2024-05-07 00:42:01 +0300

    added @Transactional into service

--- src/main/java/com/example/voteapp/service/VoteService.java
+++ src/main/java/com/example/voteapp/service/VoteService.java
@@ -5,6 +5,7 @@ import com.example.voteapp.model.FoodPlace;
 import com.example.voteapp.model.Vote;
 import com.example.voteapp.repository.FoodPlaceRepository;
 import com.example.voteapp.repository.VoteRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class VoteService {
     private final FoodPlaceRepository foodPlaceRepository;
     private final VoteRepository voteRepository;
 
+    @Transactional
     public HttpStatus vote(VoteRqDTO rqDTO){
         Optional<FoodPlace> votedFoodPlace = foodPlaceRepository.findById(rqDTO.getFoodPlaceId());
         if (votedFoodPlace.isPresent()) {
