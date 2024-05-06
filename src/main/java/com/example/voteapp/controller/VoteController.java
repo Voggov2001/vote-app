abdebc1de30458d3e18643a0a767ed18f75d5331
Ilya Voggov <voggov1@mail.ru>
2024-05-07 00:39:14 +0300

    refactored mapping

--- src/main/java/com/example/voteapp/controller/VoteController.java
+++ src/main/java/com/example/voteapp/controller/VoteController.java
@@ -4,9 +4,7 @@ import com.example.voteapp.dto.VoteRqDTO;
 import com.example.voteapp.service.VoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +12,7 @@ public class VoteController {
 
     private final VoteService voteService;
 
-    @PostMapping("/vote")
+    @PutMapping("/vote")
     public HttpStatus vote(@RequestBody VoteRqDTO rqDTO) {
         return voteService.vote(rqDTO);
     }
